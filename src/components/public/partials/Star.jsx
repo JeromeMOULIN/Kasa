5788c363e432c7116eeae7be64346812cbcdbb1b
JeromeMOULIN <j.moulin89@laposte.net>
2023-11-29 00:39:34 +0100

    Maj css stars

--- src/components/public/partials/Star.jsx
+++ src/components/public/partials/Star.jsx
@@ -6,19 +6,16 @@ const Star = (nbrStar) => {
     const [star, setStar] = useState([])
     const [emptyStar, setEmptyStar] = useState([])
 
-    const i = nbrStar.nbrStar
-
     useEffect(() =>{
-        console.log(i)
-        setStar(Array(4).fill(<i className="fa-solid fa-star "/>))
-        setEmptyStar(Array(1).fill(<i className="fa-solid fa-star grey"/>))
+        setStar(Array(2).fill(<i className="fa-solid fa-star"/>))
+        setEmptyStar(Array(3).fill(<i className="fa-solid fa-star grey"/>))
     }, [])
     
     // <i className="fa-solid fa-star "/>
     // <i className="fa-solid fa-star grey"/>
 
     return (
-        <div className='star'>
+        <div className='stars'>
          {star}{emptyStar}
         </div>
     );
