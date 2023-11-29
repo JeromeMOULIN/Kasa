b37d2559c40671bfaf5f911751c5f94c1018cdfa
JeromeMOULIN <j.moulin89@laposte.net>
2023-11-29 17:03:09 +0100

    Maj Star.jsx raccourcie du code retourné

--- src/components/public/partials/Star.jsx
+++ src/components/public/partials/Star.jsx
@@ -3,25 +3,26 @@ import React, { useEffect, useState } from 'react';
 import './star.css'
 
 const Star = (nbrRating) => {
-    const [fullStar, setFullStar] = useState([])
-    const [emptyStar, setEmptyStar] = useState([])
+    // const [fullStar, setFullStar] = useState([])
+    // const [emptyStar, setEmptyStar] = useState([])
 
     let FullStar = Array(5).fill(<i className="fa-solid fa-star" />).slice(5 - nbrRating.nbrRating)
     let EmptyStar = Array(5).fill(<i className="fa-solid fa-star grey" />).slice(nbrRating.nbrRating)
+    let star = FullStar.concat(EmptyStar)
 
-    useEffect(() => {
-        let i = nbrRating.nbrRating
+    // useEffect(() => {
+    //     let i = nbrRating.nbrRating
 
-        setFullStar(Array(5).fill(<i key={2} className="fa-solid fa-star" />).slice(5 - nbrRating.nbrRating))
-        setEmptyStar(Array(5).fill(<i key={3} className="fa-solid fa-star grey" />).slice(nbrRating.nbrRating))
-    }, [])
+    //     setFullStar(Array(5).fill(<i key={2} className="fa-solid fa-star" />).slice(5 - nbrRating.nbrRating))
+    //     setEmptyStar(Array(5).fill(<i key={3} className="fa-solid fa-star grey" />).slice(nbrRating.nbrRating))
+    // }, [])
 
     // <i className="fa-solid fa-star "/>
     // <i className="fa-solid fa-star grey"/>
 
     return (
         <div className='stars'>
-            <p><span>{FullStar}</span><span>{EmptyStar}</span></p>
+            {star}
         </div>
     );
 };
