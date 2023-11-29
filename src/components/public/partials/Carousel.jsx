8c1a030a938516f19322a4110f12e70492d09187
JeromeMOULIN <j.moulin89@laposte.net>
2023-11-29 01:17:49 +0100

    La carousel devien un composant avec comme props la transmission de la liste des images

---
+++ src/components/public/partials/Carousel.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+import './carousel.css'
+
+const Carousel = (listCarousel) => {
+    const [currentIndex, setCurrentIndex] = useState([])
+
+    useEffect(() => {
+        setCurrentIndex(0)
+    }, [])
+
+    listCarousel = listCarousel.listCarousel
+    
+    const onLeft = () => {
+        if (currentIndex <= 0) {
+            setCurrentIndex(listCarousel.length - 1)
+        } else {
+            setCurrentIndex(currentIndex - 1)
+            console.log(currentIndex)
+        }
+    }
+
+    const onRight = () => {
+        if (currentIndex >= listCarousel.length - 1) {
+            setCurrentIndex(0)
+        } else {
+            setCurrentIndex(currentIndex + 1)
+            console.log(currentIndex)
+        }
+    }
+
+    return (
+        <section className='carousel'>
+                <i onClick={onLeft} className="arrow fa-solid fa-chevron-left fa-2xl"></i>
+                <img className='imgcarousel' src={listCarousel[currentIndex]} alt="" />
+                <i onClick={onRight} className="arrow fa-solid fa-chevron-right fa-2xl"></i>
+            </section>
+    );
+};
+
+export default Carousel;
--- src/pages/public/Logement.jsx
+++ src/pages/public/Logement.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Logements from '@back/logement.json';
 import Dropdown from '@components/public/partials/Dropdown';
+import Star from '@components/public/partials/star';
+import Carousel from '@components/public/partials/Carousel';
 
 import './logement.css';
-import Star from '../../components/public/partials/star';
 
 const Logement = () => {
     const [logement, setLogement] = useState([])
@@ -12,7 +13,6 @@ const Logement = () => {
     const [carousel, setCarousel] = useState([])
     const [tags, setTags] = useState([])
     const [equipments, setEquipements] = useState([])
-    const [currentIndex, setCurrentIndex] = useState([])
 
     const { logementId } = useParams()
 
@@ -23,34 +23,14 @@ const Logement = () => {
         setCarousel(dataLogement.pictures)
         setTags(dataLogement.tags)
         setEquipements(dataLogement.equipments)
-        setCurrentIndex(0)
+
     }, [])
 
-
-    const onLeft = () => {
-        if (currentIndex <= 0) {
-            setCurrentIndex(carousel.length - 1)
-        } else {
-            setCurrentIndex(currentIndex - 1)
-            console.log(currentIndex)
-        }
-    }
-
-    const onRight = () => {
-        if (currentIndex >= carousel.length - 1) {
-            setCurrentIndex(0)
-        } else {
-            setCurrentIndex(currentIndex + 1)
-            console.log(currentIndex)
-        }
-    }
     return (
         <div className='logement'>
-            <section className='carousel'>
-                <i onClick={onLeft} className="arrow fa-solid fa-chevron-left fa-2xl"></i>
-                <img className='imgcarousel' src={carousel[currentIndex]} alt="" />
-                <i onClick={onRight} className="arrow fa-solid fa-chevron-right fa-2xl"></i>
-            </section>
+
+            <Carousel listCarousel={carousel} />
+
             <section className='mainContent'>
 
                 <div className='headerContent'>
